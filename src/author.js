f8dbae12b5e1f8c27e05d156d3d9c34a154eb631
nbyrd9 <nianibyrd@gmail.com>
2021-05-04 14:58:49 -0400

    fetch request for author form

--- src/author.js
+++ src/author.js
@@ -1,12 +1,36 @@
 class Author {
     constructor(name) {
         this.authorsUrl = "http://localhost:3000/authors"
+        this.BindingAndEventListeners()
         this.name= name;
     }
 
-    static all = []
+    BindingAndEventListeners() {
+		this.newAuthorFormDiv = document.getElementById('new-author-form-div')
+	}
 
-    static renderAuthors() {
+    getPrimaryComments() {
+		return fetch(this.authorsUrl).then(r => r.json())
+	}
+
+    createAuthor(jsObj){
+        const author = {
+            name: jsObj.name
+        }
+        return fetch(this.authorsUrl, {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(author)
+			})
+		.then(response => {
+			return response.json()
+		})
+    }
+
+
+    renderNewAuthorForm() {
         
 }
 
